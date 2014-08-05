9af70221dcd2b22ee8a7a2f109bb1459aad2b498
Justin Kim <justinjmkim@gmail.com>
2014-08-05 18:14:39 -0400

    what we have so far

--- app2.js
+++ app2.js
@@ -2,7 +2,11 @@ var socket = require('socket.io')
   , http = require('http')
   , twitter = require('ntwitter')
   , express = require('express')
+  , path = require('path')
+  , util = require('util')
   , app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 var server = http.createServer(app);
 var io = socket.listen(server);
@@ -16,18 +20,33 @@ var t = new twitter({
 
 app.set('port', process.env.PORT || 8080);
 
-// app.get('/', function(request, response) {
+app.get('/', function(request, response) {
   console.log('This half works...');
-  io.sockets.on('connection', function(client) {
-    console.log('Server is connected...');
-    // t.stream('statuses/sample',function(stream) {
-    //   stream.on('data', function(tweet) {
-    //     if (tweet.geo !== null) {
-    //        io.sockets.emit('tweets', {text: tweet});
-    //     }
-    //   });
-    // });
+  response.sendfile('views/index.html');
+});
+
+io.sockets.on('connection', function(client) {
+  console.log('Server is connected...');
+  client.on('error', function() {
+    console.log('error catch!');
   });
-// });
+  t.stream('statuses/sample',function(stream) {
+    stream.on('data', function(tweet) {
+      if (tweet.geo !== null) {
+         client.emit('tweets', {text: tweet});
+      }
+    });
+    stream.on('end', function (response) {
+      // Handle a disconnection
+    });
+    stream.on('destroy', function (response) {
+      // Handle a 'silent' disconnection from Twitter, no end/error event fired
+    });
+    stream.on('error', function(error) {
+      console.log('Tweet errror');
+      console.log(util.inspect(error));
+    });
+  });
+});
 
 server.listen(8080);
