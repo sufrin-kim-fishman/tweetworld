d6f51ad7837739d22a14e73c5961849363715b0f
github username <github email address>
2014-08-06 15:25:56 -0400

    not workin tho

--- public/javascripts/script2.js
+++ public/javascripts/script2.js
@@ -1,17 +1,19 @@
 $(function(){
   var server = io.connect('http://localhost:8080');
+  countriesToShow = [];
   var country;
   $("#submit").click(function(e) {
   	console.log("hi");
-  	country = $("#country").val();
-  	e.preventDefault();
+  	  country = $("#country").val();
+      countriesToShow.push(country);
+  	  e.preventDefault();
   });
   server.on('tweets', function(data) {
-    if(country === data.place.country) {
-    	if($("#" + country).length === 0) {
-    		$(".tweet_country_holder").append("<div id='"+ country +"'><ul></ul></div>");
-    	}
-    	insertTweet(data, country);
+    if(countriesToShow.indexOf(country) >= 0) {
+      if($("#" + country).length === 0) {
+      	$(".tweet_country_holder").append("<div id='"+ country +"'><ul></ul></div>");
+      }
+      insertTweet(data, country);
     }
   });
 });
@@ -19,7 +21,7 @@ $(function(){
 
 
 function insertTweet(tweet, country) {
- if  ($("#" + country +" > li").length > 10) {
+ if  ($("#" + country +" > li").length > 9) {
   console.log("deleting top tweet...");
   console.log($("#" + country +" li").last());
   $("#" + country +" li").last().remove();
