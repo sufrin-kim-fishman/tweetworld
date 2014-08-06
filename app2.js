2dfb316ff89599675eb48f63b744745077936af5
ilanasufrin <ilanasufrin@gmail.com>
2014-08-06 18:32:52 -0400

    trying to get all countries

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 var socket = require('socket.io')
   , http = require('http')
-  , twitter = require('ntwitter')
+  , twitter = require('twit')
   , express = require('express')
   , path = require('path')
   , util = require('util')
@@ -14,7 +14,7 @@ var io = socket.listen(server);
 var t = new twitter({
     consumer_key: "k8Owe3iZwGMUgD1Dn64lTKN32",          
     consumer_secret: "6HhPFD747pq9iXbHNoqCMclqGa1Ibd4IhvWC7h6bBY8h7hp762",       
-    access_token_key: "942399211-7J7mUWuEC1shJHXRnFvKyEFUJVhRh9aFfQd43BzB",      
+    access_token: "942399211-7J7mUWuEC1shJHXRnFvKyEFUJVhRh9aFfQd43BzB",      
     access_token_secret: "EPXwymTXCdTWkMzcxlnVYsI0Gexxo4mB1505idQ40jb9k"
 });
 
