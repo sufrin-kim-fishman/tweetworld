ee92591bd901c6b0687b8d2109d0a36b499dfc74
Justin Kim <justinjmkim@gmail.com>
2014-08-07 00:46:42 -0400

    Make global name normalization, refactor code into methods, and rename files and directories

--- app.js
+++ app.js
@@ -1,151 +1,59 @@
-/**
- * Module dependencies.
- */
-var express = require('express')
-  , io = require('socket.io')
+var socket = require('socket.io')
   , http = require('http')
-  , twitter = require('ntwitter')
-  // , cronJob = require('cron').CronJob
-  , _ = require('underscore')
+  , twitter = require('twit')
+  , express = require('express')
   , path = require('path')
-  , morgan = require('morgan')
-  , bodyParser = require('body-parser')
-  , methodOverride = require('method-override')
-  , errorHandler = require('errorhandler');
+  , app = express();
 
-//Create an express app
-var app = express();
-
-//Create the HTTP server with the express app as an argument
-var server = http.createServer(app);
-
-// Twitter symbols array
-// var watchSymbols = ['$msft', '$intc', '$hpq', '$goog', '$nok', '$nvda', '$bac', '$orcl', '$csco', '$aapl', '$ntap', '$emc', '$t', '$ibm', '$vz', '$xom', '$cvx', '$ge', '$ko', '$jnj'];
-
-//This structure will keep the total number of tweets received and a map of all the symbols and how many tweets received of that symbol
-// var watchList = {
-//     total: 0,
-//     symbols: {}
-// };
-
-//Set the watch symbols to zero.
-// _.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
-
-//Generic Express setup
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(morgan('dev'));
-app.use(bodyParser());
-app.use(methodOverride());
-app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//We're using bower components so add it to the path to make things easier
-app.use('/components', express.static(path.join(__dirname, 'components')));
+var server = http.createServer(app);
+var io = socket.listen(server);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(errorHandler());
-}
-
-//Our only route! Render it with the current watchList
-// app.get('/', function(req, res) {
-//   res.render('index', { data: watchList });
-// });
-
-//Start a Socket.IO listen
-var sockets = io.listen(server);
-
-//Set the sockets.io configuration.
-// THIS IS NECESSARY ONLY FOR HEROKU!
-// sockets.configure(function() {
-//   sockets.set('transports', ['xhr-polling']);
-//   sockets.set('polling duration', 10);
-// });
-
-//If the client just connected, give them fresh data!
-// sockets.sockets.on('connection', function(socket) { 
-//     socket.emit('data', watchList);
-// });
-
-//Instantiate the twitter component
-//You will need to get your own key. Don't worry, it's free. But I cannot provide you one
-//since it will instantiate a connection on my behalf and will drop all other streaming connections.
-//Check out: https://dev.twitter.com/
 var t = new twitter({
     consumer_key: "k8Owe3iZwGMUgD1Dn64lTKN32",          
     consumer_secret: "6HhPFD747pq9iXbHNoqCMclqGa1Ibd4IhvWC7h6bBY8h7hp762",       
-    access_token_key: "942399211-7J7mUWuEC1shJHXRnFvKyEFUJVhRh9aFfQd43BzB",      
+    access_token: "942399211-7J7mUWuEC1shJHXRnFvKyEFUJVhRh9aFfQd43BzB",      
     access_token_secret: "EPXwymTXCdTWkMzcxlnVYsI0Gexxo4mB1505idQ40jb9k"
 });
 
-//Tell the twitter API to filter on the watchSymbols 
-// t.stream('statuses/sample', function(stream) {
+app.set('port', process.env.PORT || 8080);
 
-//   //We have a connection. Now watch the 'data' event for incomming tweets.
-//   stream.on('data', function(tweet) {
-
-//     //This variable is used to indicate whether a symbol was actually mentioned.
-//     //Since twitter doesnt why the tweet was forwarded we have to search through the text
-//     //and determine which symbol it was ment for. Sometimes we can't tell, in which case we don't
-//     //want to increment the total counter...
-//     var claimed = false;
-
-//     //Make sure it was a valid tweet
-//     if (tweet.text !== undefined) {
-
-//       //We're gunna do some indexOf comparisons and we want it to be case agnostic.
-//       var text = tweet.text.toLowerCase();
-
-//       //Go through every symbol and see if it was mentioned. If so, increment its counter and
-//       //set the 'claimed' variable to true to indicate something was mentioned so we can increment
-//       //the 'total' counter!
-//       // _.each(watchSymbols, function(v) {
-//       //     if (text.indexOf(v.toLowerCase()) !== -1) {
-//       //         watchList.symbols[v]++;
-//       //         claimed = true;
-//       //     }
-//       // });
-
-//       // //If something was mentioned, increment the total counter and send the update to all the clients
-//       // if (claimed) {
-//       //     //Increment total
-//       //     watchList.total++;
-
-//       //     //Send to all the clients
-//       //     sockets.sockets.emit('data', watchList);
-//       // }
-//       sockets.sockets.emit('data', text);
-//     }
-//   });
-// });
-
-app.get('/', function(request, response) {
-  t.stream('statuses/sample',function(stream) {
-    stream.on('data', function(tweet) {
-      if (tweet.geo !== null) {
-         // sockets.sockets.emit('data', tweet.text);
-         tweet.pipe(response);
-      }
-    });
+function setPage() {
+  app.all('/', function(request, response) {
+    response.sendfile('views/index.html');
   });
-});
+}
 
-//Reset everything on a new day!
-//We don't want to keep data around from the previous day so reset everything.
-// new cronJob('0 0 0 * * *', function(){
-    // //Reset the total
-    // watchList.total = 0;
+function openTweetConnection() {
+  io.sockets.on('connection', function(client) {
+    catchError(client);
+    streamTweets(client);
+  });
+}
 
-    // //Clear out everything in the map
-    // _.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
+function catchError(client) {
+  client.on('error', function() {
+    console.log('error catch!');
+  });
+}
 
-    // //Send the update to the clients
-    // sockets.sockets.emit('data', watchList);
-// }, null, true);
+function streamTweets(client) {
+  var stream = t.stream('statuses/sample');
+  stream.on('tweet', function(tweet) {
+    if (tweet.geo !== null) {
+      console.log(tweet);
+      client.emit('tweets', JSON.stringify(tweet));
+    }
+  });
+}
 
-//Create the server
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+function listenToServer() {
+  server.listen(8080);
+}
+
+(function() {
+  setPage();
+  openTweetConnection();
+  listenToServer();
+})()
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,47 @@
-$(function() {
-    var socket = io.connect(window.location.hostname);
-    socket.on('data', function(data) {
-        var total = data.total;
-        for (var key in data.symbols) {
-            var val = data.symbols[key] / total;
-            if (isNaN(val)) {
-                val = 0;
-            }
+var server = io.connect('http://localhost:8080');
+var countriesToShow = [];
 
-            $('li[data-symbol="' + key + '"]').each(function() {
-                $(this).css('background-color', 'rgb(' + Math.round(val * 255) +',0,0)');
-            });
-        }
-        $('#last-update').text(new Date().toTimeString());
-    });
+$(function(){
+  submitListener();
 });
+
+function insertTweet(tweet, country) {
+  var $countryUl = $("#" + country + " ul");
+  if  ($countryUl.children().length > 9) {
+    $("#" + country +" li").last().remove();
+  }
+ $countryUl.prepend('<li>' + tweet.text + '</li>');
+}
+
+function normalizeName(name) {
+  return name.split(" ").join("-");
+}
+
+function submitListener() {
+  $("#submit").click(function(e) {
+      var country = normalizeName($("#country").val());
+      countriesToShow.push(country);
+      console.log(countriesToShow);
+      e.preventDefault();
+  });
+}
+
+(function tweetListener() {
+  server.on('tweets', function(data) {
+    data = JSON.parse(data);
+    var normalizedCountry = normalizeName(data.place.country);
+    console.log(normalizedCountry);
+    if(countriesToShow.indexOf(normalizedCountry) >= 0) {
+      addNewCountry(data, normalizedCountry);
+      insertTweet(data, normalizedCountry);
+    }
+  });
+})()
+
+function addNewCountry(data, country) {
+  if($("#" + country).length === 0) {
+    var newCountry = "<div id='" + country + "'>" + 
+      "<h2>" + data.place.country + "</h2><ul></ul></div>"
+    $(".tweet_country_holder").append(newCountry);
+  }
+}
