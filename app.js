33a7a3c58283909ad97032ec65c4e6c5a5bbe0c8
ilanasufrin <ilanasufrin@gmail.com>
2014-08-07 17:05:40 -0400

    WE HAVE POSTGRES AND IT STORES COUNTRIES

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var socket = require('socket.io')
   , app = express();
 
 var pg = require('pg');
-var conString = "postgres://ilanasufrin:@localhost:5432/test";
+//configure this using your local postgres settings
+var conString = "postgres://ilanasufrin:@localhost:5432/TweetWorld";
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,8 +47,9 @@ function catchError(client) {
 
 function streamTweets(client) {
   stream.on('tweet', function(tweet) {
-    if (tweet.geo !== null) {
+    if (tweet.place !== null) {
       console.log(tweet);
+      insertIntoDatabase(tweet.place.country);
       client.emit('tweets', JSON.stringify(tweet));
     }
   });
@@ -67,21 +72,53 @@ function listenToServer() {
 })()
 
 
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('INSERT INTO tristans_test (code) VALUES ($1)', ['THIS IS FROM NODE!!!'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+// pg.connect(conString, function(err, client, done) {
+//   if(err) {
+//     return console.error('error fetching client from pool', err);
+//   }
+//   client.query('INSERT INTO tristans_test (code) VALUES ($1)', ['THIS IS FROM NODE!!!'], function(err, result) {
+  
+//     //call `done()` to release the client back to the pool
+//     done();
 
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result);
+//     //output: 1
+//   });
+// });
+
+
+  //RUN THIS LOCALLY: create database "TweetWorld";
+function insertIntoDatabase(tweet) {
+  pg.connect(conString, function(err, client, done) {
     if(err) {
-      return console.error('error running query', err);
+      return console.error('error fetching client from pool', err);
     }
-    console.log(result);
+
+    client.query('CREATE TABLE IF NOT EXISTS countryNames ("id" SERIAL PRIMARY KEY, "name" varchar(200));', function(err, result) {
+      console.log("created table or using one that exists. good.");
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('problem creating table', err);
+      }
+    });
+
+    client.query("INSERT INTO countryNames (name) VALUES ('" + tweet + "');", function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('problem inserting country name', err);
+      }
+      console.log(result);
     //output: 1
+    });
   });
-});
+}
 
 
 
