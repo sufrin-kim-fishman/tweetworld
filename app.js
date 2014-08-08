e2c1127004bdb84d509d729c7958b0c98202e86f
ilanasufrin <ilanasufrin@gmail.com>
2014-08-08 15:40:58 -0400

    fixing merge conflicts

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var socket = require('socket.io')
   , app = express()
   , db   = require('./models');
 
-//configure this using your local postgres settings
 
 //RUN THIS LOCALLY: create database "TweetWorld";
 //var conString = "postgres://ilanasufrin:@localhost:5432/TweetWorld";
@@ -22,9 +21,14 @@ var socket = require('socket.io')
 //run this: 
 //npm install --save pg
 //npm install --save sequelize-cli
-//configure this using your local postgres settings
 
 
+
+
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
 
 
 app.use(session({secret: 'topsecretsecret',
@@ -66,19 +70,13 @@ var server = http.createServer(app);
 var io = socket.listen(server);
 
 var t = new twitter({
-    consumer_key: "N3wxjYdnjaq8OjmlyeAnQ0uiX",          
-    consumer_secret: "C2Y71NQyrFBCbGHhkSFPqHE9penIacIvB1DUgrk7jAkUW8qpmM",       
-    access_token: "316015598-IciiwWwpx7tf0HaslKVu2SL5otrONTzZOJwG0TnR",      
-    access_token_secret: "9Ju0fDyFZ2ksMf9YTwDJlO7csqyecrBs3pwtBclQqyjOg"
+    consumer_key: "35AidvtI1yk6AKcNc6BDoMcVs",          
+    consumer_secret: "ZhrapDlProEE6zZya4g1QdZjkfv9Q6HTBG7Q2Oy5TkGSXjihcD",       
+    access_token: "2453054691-taj0rqSb33InlsEgkxEG2JSSxl546vWRt0QnkyH",      
+    access_token_secret: "gUI8lk4GQVIAZ7zzUJ61s1XyGvx6D8oGO2ECGW8ZZsd1A"
 });
 var stream = t.stream('statuses/sample');
 app.set('port', process.env.PORT || 9000);
-
-function setPage() {
-  app.get('/', function(request, response) {
-    response.sendfile('views/index.html');
-  });
-}
 
 function openTweetConnection() {
   io.sockets.on('connection', function(client) {
@@ -108,81 +106,11 @@ function listenToServer() {
 }
 
 (function() {
-  setPage();
   openTweetConnection();
   listenToServer();
+<<<<<<< HEAD
 })()
 
-
-//   //RUN THIS LOCALLY: create database "TweetWorld";
-// function doDatabaseThings(tweet) {
-//  createTable();
-//  findRowName(tweet);
-// }
-
-// function createTable() {
-//    pg.connect(conString, function(err, client, done) {
-//     if(err) {
-//       return console.error('error fetching client from pool', err);
-//     }
-
-//     client.query('CREATE TABLE IF NOT EXISTS countryNames ("id" SERIAL PRIMARY KEY, "name" varchar(200));', function(err, result) {
-//       console.log("created table or using one that exists. good.");
-//       //call `done()` to release the client back to the pool
-//       done();
-
-//       if(err) {
-//         return console.error('problem creating table', err);
-//       }
-//     });
-//   });
-// }
-
-// function insertRecord(tweet) {
-//    pg.connect(conString, function(err, client, done) {
-//     if(err) {
-//       return console.error('error fetching client from pool', err);
-//     }
-
-//     client.query("INSERT INTO countryNames (name) VALUES ('" + tweet + "');", function(err, result) {
-//       //call `done()` to release the client back to the pool
-//       done();
-
-//       if(err) {
-//         return console.error('problem inserting country name', err);
-//       }
-//       console.log(result);
-//     //output: 1
-//     });
-//   });
-// }
-
-// function findRowName(name) {
-//   pg.connect(conString, function(err, client, done) {
-//     if(err) {
-//       return console.error('problems connecting', err);
-//     }
-
-
-//     client.query("SELECT * from countryNames where name = '" + name + "';", function(err, result) {
-//       //call `done()` to release the client back to the pool
-//       console.log("THIS IS WHAT GETS RETURNED: " + result);
-//       if(result == false) {
-//         insertRecord(tweet);
-//       }
-//       done();
-
-//       if(err) {
-//         return console.error('problem finding country name', err);
-//       }
-//       console.log(result);
-
-//       //true or false here
-      
-//    });
-//   });
-
-// }
 
 
 
