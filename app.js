8e68270c1a2f668e9433f7625c9419e6d03f7149
Justin Kim <justinjmkim@gmail.com>
2014-08-11 10:43:02 -0400

    Put in apikeys

--- app.js
+++ app.js
@@ -45,10 +45,6 @@ var io = env.socket.listen(server);
 var client;
 
 var t = new env.twitter({
-    // consumer_key: "35AidvtI1yk6AKcNc6BDoMcVs",          
-    // consumer_secret: "ZhrapDlProEE6zZya4g1QdZjkfv9Q6HTBG7Q2Oy5TkGSXjihcD",       
-    // access_token: "2453054691-taj0rqSb33InlsEgkxEG2JSSxl546vWRt0QnkyH",      
-    // access_token_secret: "gUI8lk4GQVIAZ7zzUJ61s1XyGvx6D8oGO2ECGW8ZZsd1A"
     consumer_key: apikeys.consumer_key,          
     consumer_secret: apikeys.consumer_secret,       
     access_token: apikeys.access_token,      
@@ -122,6 +118,7 @@ function persistCountry(countryName, username) {
 
 function listenToServer() {
   server.listen(env.port);
+  server.listen('8080/dashboard');
 }
 
 (function() {
