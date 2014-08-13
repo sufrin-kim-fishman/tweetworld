ba7dbc71a9ebe9d121bf16b25fc322deaa34f4a8
Justin Kim <justinjmkim@gmail.com>
2014-08-13 16:55:28 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -4,11 +4,6 @@ module.exports = function() {
     consumer_secret: "C2Y71NQyrFBCbGHhkSFPqHE9penIacIvB1DUgrk7jAkUW8qpmM",       
     access_token: "316015598-IciiwWwpx7tf0HaslKVu2SL5otrONTzZOJwG0TnR",      
     access_token_secret: "9Ju0fDyFZ2ksMf9YTwDJlO7csqyecrBs3pwtBclQqyjOg",
-    session_secret: "topsecretsecret",
-    username: "Ado",
-    password: null,
-    host: "yzmaxbfjqgoqxr:IzZnKk9NoQWxN9GWQsoAm5VbW1@ec2-54-225-101-199.compute-1.amazonaws.com:5432/dbgvojp417uj1l",
-    database_url: "HEROKU_POSTGRESQL_BLUE_URL",
-    database_local: "TweetWorld"
+    session_secret: "topsecretsecret"
   };
 };
