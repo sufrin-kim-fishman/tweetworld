44425fd3dbec6317070ec339de0b47e3aaea2742
Justin Kim <justinjmkim@gmail.com>
2014-08-13 14:29:18 -0400

    Re add database methods in

--- models/index.js
+++ models/index.js
@@ -18,11 +18,27 @@ if (!global.hasOwnProperty('db')) {
     });
 
   } else {
-    sequelize = new Sequelize('TweetWorld', 'justinkim', null, {
+    sequelize = new Sequelize('TweetWorld', 'justinkim', "", {
       dialect: "postgres",
       port:    5432
     });
   }
+
+  fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+      return (file.indexOf('.') !== 0) && (file !== 'index.js')
+    })
+    .forEach(function(file) {
+      var model = sequelize.import(path.join(__dirname, file))
+      db[model.name] = model
+    })
+   
+  Object.keys(db).forEach(function(modelName) {
+    if ('associate' in db[modelName]) {
+      db[modelName].associate(db)
+    }
+  })
  
   global.db = {
     Sequelize: Sequelize,
