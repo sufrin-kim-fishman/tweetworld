26b37d99780e680f371a4eb34891d2419be94214
Justin Kim <justinjmkim@gmail.com>
2014-08-13 01:10:51 -0400

    countries persist and can be deleted

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -17,17 +17,18 @@ function normalizeName(name) {
 
 function submitListener() {
   $("#submit").click(function(e) {
-    var $country = states[$("#country").val()];
+    var countryName = $("#country").val();
+    var $country = states[countryName];
     var country = normalizeName($country);
-    addNewCountry($country, country);
+    addNewCountry(country, countryName);
     e.preventDefault();
   });
 }
 
-function addNewCountry($country, country) {
+function addNewCountry(country, countryName) {
   if($("#" + country).length === 0) {
     var newCountry = "<div id='" + country + "' class='col-md-4' style='background-color:white;border:2px;border-style:dashed;border-color:gray;border-radius:5%;margin:auto;float:left;'>" + 
-      "<h2>" + $("#country").val() + "</h2>"+
+      "<h2>" + countryName + "</h2>"+
       '<button type="button" class="delete">X</button>' +
       "<ul></ul></div>";
     $(".tweet_country_holder").append(newCountry);
